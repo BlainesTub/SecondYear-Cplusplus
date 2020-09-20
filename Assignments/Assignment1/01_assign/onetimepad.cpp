b3853d326a7c1befca9e4d2f5c6f1060c47f84ec
BlainesTub <the1stpotato@gmail.com>
2020-09-20 16:53:50 -0700

    assignment1

---
+++ Assignments/Assignment1/01_assign/onetimepad.cpp
@@ -0,0 +1,78 @@
+// add your personal information below, including your student id
+
+#include <iostream>
+#include <string>
+
+std::string onetimepad(std::string auth, std::string input)
+{
+    //ERROR CATCHER
+    if (input.size() > auth.size())
+    {
+        std::cout << "Error: the length of the key [" << auth.size()
+                  << "] is smaller than the length of the message to encrypt ["
+                  << input.size() << "]" << std::endl;
+        exit(1);
+    }
+    std::string encrypted = input; //Set length of encrypted String.
+    for (int i = 0; i < (int(input.size())); ++i)
+    {
+        encrypted.at(i) = input.at(i) ^ auth.at(i);
+    }
+    return encrypted;
+}
+
+int main()
+{
+    //initialize variables 
+    std::string key;
+    std::string message;
+    std::string encrypted;
+    std::string decrypted;
+    
+    //Acquire & Print Key
+    std::getline(std::cin, key);
+    std::cout<<"Original key ["<< key << "]"<< std::endl;
+    
+    //Loop while input is valid
+    while(std::getline(std::cin, message)){
+
+        
+        
+        std::cout<<"Original message ["<< message << "]" << std::endl;
+        
+        encrypted = onetimepad(key,message);  //encrypt the message
+
+        {std::cout << "Encrypted ["; //print the encrypted message
+            for (int i = 0; i < int(encrypted.size()); ++i)
+            {
+                if (char(encrypted.at(i)) < 32 || char(encrypted.at(i) > 126))
+                {
+                    std::cout << "(" << int(encrypted.at(i)) << ")";
+                }
+                else
+                {
+                    std::cout << encrypted.at(i);
+                }
+            }
+            std::cout << "]" << std::endl;
+        }
+
+        decrypted = onetimepad(key,encrypted);
+        {std::cout << "Decrypted ["; //print the Decrypted message
+            for (int i = 0; i < int(decrypted.size()); ++i)
+            {
+                if (char(decrypted.at(i)) < 32 || char(decrypted.at(i) > 126))
+                {
+                    std::cout << "(" << int(decrypted.at(i)) << ")";
+                }
+                else
+                {
+                    std::cout << decrypted.at(i);
+                }
+            }
+            std::cout << "]" << std::endl;
+        }
+    }
+
+    return 0;
+}
